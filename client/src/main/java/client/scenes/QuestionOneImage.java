7a701ee7b2fad4180019afd755542042c221fb23
Mirella Günther <M.Gunther-1@student.tudelft.nl>
2022-03-01 22:17:00 +0100

    Disable other answer buttons once one answer is selected

--- client/src/main/java/client/scenes/QuestionOneImage.java
+++ client/src/main/java/client/scenes/QuestionOneImage.java
@@ -1,10 +1,12 @@
 package client.scenes;
 
+import client.scenes.controllerrequirements.QuestionRequirements;
+import commons.Question;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javax.inject.Inject;
 
-public class QuestionOneImage {
+public class QuestionOneImage implements QuestionRequirements {
 
     private MainCtrl mainCtrl;
     private QuestionFrameCtrl questionFrameCtrl;
@@ -30,23 +32,46 @@ public class QuestionOneImage {
         this.questionFrameCtrl = questionFrameCtrl;
     }
 
+    /**
+     * Sets the text on Button A to 'Clicked!' and disables buttons B and C
+     */
     @FXML
     public void setAnswerA() {
         answerA.setText("Clicked!");
+        answerB.setOnAction(null);
+        answerC.setOnAction(null);
     }
 
     ;
-
+    /**
+     * Sets the text on Button B to 'Clicked!' and disables buttons A and C
+     */
     @FXML
     public void setAnswerB() {
         answerB.setText("Clicked!");
+        answerA.setOnAction(null);
+        answerC.setOnAction(null);
     }
 
     ;
-
+    /**
+     * Sets the text on Button C to 'Clicked!' and disables buttons A and B
+     */
     @FXML
     public void setAnswerC() {
         answerC.setText("Clicked!");
+        answerA.setOnAction(null);
+        answerB.setOnAction(null);
+    }
+
+    @Override
+    public void initialize(Question question) {
+
+    }
+
+    @Override
+    public void revealCorrectAnswer(Question question) {
+
     }
 
     ;
