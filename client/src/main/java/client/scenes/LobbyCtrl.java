d906fc11fc3923be68769ed9514f58458efc5c0c
Yannick Zuurbier <y.r.zuurbier@student.tudelft.nl>
2022-03-04 20:18:22 +0100

    CSS FXML Java for Lobby
    I hope everything is alright, it worked on my pc but I would make sure cause I'm not very good at coding D:

---
+++ client/src/main/java/client/scenes/LobbyCtrl.java
@@ -0,0 +1,76 @@
+package client.scenes;
+
+import client.scenes.controllerrequirements.LobbyCtrlRequirements;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+
+
+import javax.xml.transform.Templates;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LobbyCtrl implements Initializable, LobbyCtrlRequirements {
+
+    @FXML
+    private TableColumn<String, String> name;
+
+    @FXML
+    private TableView<String> table;
+
+    @FXML
+    void initializeMultiplayerGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void goBack(ActionEvent actionEvent) {
+
+    }
+
+    ObservableList<String> list = FXCollections.observableArrayList(
+            "Yannick",
+            "Per",
+            "Irina",
+            "Andrei",
+            "Mirella",
+            "Chris"
+    );
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        table.setItems(list);
+    }
+
+    @Override
+    public void addPlayer(String name) {
+        list.add(name);
+    }
+
+    @Override
+    public void removePlayer(String name) {
+        int r = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(name)) {
+                r = i;
+            }
+        }
+        list.remove(r);
+    }
+
+    @Override
+    public void clearPlayers() {
+        list.clear();
+    }
+}
---
+++ client/src/test/java/client/scenes/LobbyCtrlTest.java
@@ -0,0 +1,63 @@
+package client.scenes;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LobbyCtrlTest {
+
+    //There is probably more to test but I'm not sure what that would be tbh, I can do that later
+
+    @Test
+    public void removePersonTest() {
+        removePlayer("Yannick");
+        assertTrue(list.get(0).equals("Per"));
+    }
+
+    @Test
+    public void addPersonTest() {
+        addPlayer("John");
+        assertTrue(list.get(6).equals("John"));
+    }
+
+    @Test
+    public void clearTest() {
+        clearPlayers();
+        assertTrue(list.isEmpty());
+    }
+
+    //The methods because I can't detect them normally for some reason
+
+    private void removePlayer(String name) {
+        int r = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(name)) {
+                r = i;
+            }
+        }
+        list.remove(r);
+    }
+
+    public void addPlayer(String name) {
+        list.add(name);
+    }
+
+    public void clearPlayers() {
+        list.clear();
+    }
+
+    ObservableList<String> list = FXCollections.observableArrayList(
+            "Yannick",
+            "Per",
+            "Irina",
+            "Andrei",
+            "Mirella",
+            "Chris"
+    );
+}
