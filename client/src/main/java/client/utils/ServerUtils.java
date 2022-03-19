417b8a2e98c1a43c19260879ecd21d881f67f4c1
Andrei Visoiu <a.i.visoiu@student.tudelft.nl>
2022-03-19 15:39:08 +0100

    Add methods related to activities + support for empty "server ip" field (view description)
    Add methods to delete and update a specific activity from the database in ServerUtils
    If the user leaves the "Server IP" field empty in the main frame, the application will default to localhost:8080

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -18,6 +18,7 @@ package client.utils;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
+import commons.Activity;
 import commons.Game;
 import commons.LeaderboardEntry;
 import commons.Lobby;
@@ -29,7 +30,8 @@ import java.util.UUID;
 import org.glassfish.jersey.client.ClientConfig;
 
 /**
- * Not relevant for now
+ * This class contains all the utils for communicating with the server
+ * (e.g. all the functions the client will use to send/request data from the server)
  */
 public class ServerUtils {
 
@@ -51,7 +53,7 @@ public class ServerUtils {
      */
     public void setServerIP(String serverIP) {
         if (validateIP(serverIP)) {
-            this.serverIP = serverIP;
+            this.serverIP = parseIP(serverIP);
         }
     }
 
@@ -63,10 +65,21 @@ public class ServerUtils {
      */
     public boolean validateIP(String serverIP) {
         return ClientBuilder.newClient(new ClientConfig()) //
-            .target(serverIP).path("api/game/validate") //
+            .target(parseIP(serverIP)).path("api/game/validate") //
             .request(APPLICATION_JSON) //
             .accept(APPLICATION_JSON) //
             .get(String.class).equals("Connected");
+    }
+
+    /**
+     * Lets the user input no IP when trying to use the app.
+     * If the "Server IP" field is empty, localhost:8080 will be the default.
+     *
+     * @param serverIP the IP to check
+     * @return either the same IP, if the IP is not empty, or "http://localhost:8080/" otherwise.
+     */
+    private String parseIP(String serverIP) {
+        return (serverIP.length() != 0) ? serverIP : "http://localhost:8080/";
     }
 
     /**
@@ -123,6 +136,12 @@ public class ServerUtils {
             .get(Game.class);
     }
 
+    /**
+     * Fetches the first <code>limit</code> entries sorted by score from the leaderboard.
+     *
+     * @param limit the number of entries to fetch
+     * @return a list of leaderboard entries, at most <code>limit</code> in number, sorted by the score.
+     */
     public List<LeaderboardEntry> getSoloLeaderboard(int limit) {
         return ClientBuilder.newClient(new ClientConfig()) //
             .target(serverIP).path("api/leaderboard/" + limit) //
@@ -132,6 +151,13 @@ public class ServerUtils {
             });
     }
 
+
+    /**
+     * Adds a new entry to the leaderboard after a singleplayer game.
+     *
+     * @param entry a LeaderboardEntry object, which will contain the name of the player and their score
+     * @return the entry that was just added
+     */
     public LeaderboardEntry addLeaderboardEntry(LeaderboardEntry entry) {
         return ClientBuilder.newClient(new ClientConfig()) //
             .target(serverIP).path("api/leaderboard/add") //
@@ -156,5 +182,48 @@ public class ServerUtils {
 
     public void disconnect(UUID gameId, LeaderboardEntry player) {
         // TODO send data to server that the player disconnected
+    }
+
+
+    /**
+     * Fetch the list of all the activities stored in the database
+     *
+     * @return a list of activities, containing all the activities stored in the DB
+     */
+    public List<Activity> fetchActivities() {
+        return ClientBuilder.newClient(new ClientConfig()) //
+            .target(serverIP).path("api/activities/") //
+            .request(APPLICATION_JSON)
+            .accept(APPLICATION_JSON)
+            .get(new GenericType<>() {
+            });
+    }
+
+    /**
+     * Updates an activity in the database.
+     *
+     * @param activity the new activity object
+     * @return the same object on success
+     */
+    public Activity sendActivityUpdate(Activity activity) {
+        return ClientBuilder.newClient(new ClientConfig()) //
+            .target(serverIP).path("api/activities/update") //
+            .request(APPLICATION_JSON) //
+            .accept(APPLICATION_JSON) //
+            .post(Entity.entity(activity, APPLICATION_JSON), Activity.class);
+    }
+
+    /**
+     * Deletes an activity from the DB, by ID.
+     *
+     * @param activity the activity object to delete from the database
+     * @return an Activity instance equal to the activity that was just deleted
+     */
+    public Activity deleteActivity(Activity activity) {
+        return ClientBuilder.newClient(new ClientConfig()) //
+            .target(serverIP).path("api/activities/del") //
+            .request(APPLICATION_JSON) //
+            .accept(APPLICATION_JSON) //
+            .post(Entity.entity(activity, APPLICATION_JSON), Activity.class);
     }
 }
