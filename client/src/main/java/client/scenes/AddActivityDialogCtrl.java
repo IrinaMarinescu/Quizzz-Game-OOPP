e2423358dbf5f252a8f7dbbbf84ae0ee3f296903
Andrei Visoiu <a.i.visoiu@student.tudelft.nl>
2022-03-29 16:12:13 +0200

    Add JavaDoc and controller requirements

--- client/src/main/java/client/scenes/AddActivityDialogCtrl.java
+++ client/src/main/java/client/scenes/AddActivityDialogCtrl.java
@@ -1,5 +1,6 @@
 package client.scenes;
 
+import client.scenes.controllerrequirements.AddActivityDialogCtrlRequirements;
 import client.scenes.exceptions.InvalidDataException;
 import client.utils.ServerUtils;
 import commons.Activity;
@@ -18,7 +19,7 @@ import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 import javax.inject.Inject;
 
-public class AddActivityDialogCtrl {
+public class AddActivityDialogCtrl implements AddActivityDialogCtrlRequirements {
 
     @FXML
     private Button cancelButton;
@@ -49,22 +50,38 @@ public class AddActivityDialogCtrl {
         this.serverUtils = serverUtils;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @FXML
     public void closeDialog() {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param stage the stage to bind the scene to
+     */
     public void reset(Stage stage) {
         this.stage = stage;
         errorText.setVisible(false);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param text the text to show in the error area.
+     */
     public void showErrorText(String text) {
         errorText.setText(text);
         errorText.setVisible(true);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @FXML
     public void openFileChooser() {
         try {
@@ -84,6 +101,13 @@ public class AddActivityDialogCtrl {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param file the file the user has uploaded
+     * @throws InvalidDataException exception thrown if the file has some invalid properties (extension, size)
+     * @throws IOException exception thrown if there were problems reading the file
+     */
     public void validateFileProperties(File file) throws InvalidDataException, IOException {
         boolean equalsOne = false;
         Optional<String> extension = getExtension(file.getName());
@@ -111,12 +135,22 @@ public class AddActivityDialogCtrl {
         }
     }
 
+    /**
+     * Internal helper function. Returns the extension of a file based on the name.
+     *
+     * @param filename the name of the file to return the extension for.
+     * @return an Optional containing a String, that might or might not be present
+     * depending on whether the file name has an extension or not.
+     */
     private Optional<String> getExtension(String filename) {
         return Optional.ofNullable(filename)
             .filter(f -> f.contains("."))
             .map(f -> f.substring(filename.lastIndexOf(".")));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @FXML
     public void addActivity() {
         try {
@@ -132,6 +166,12 @@ public class AddActivityDialogCtrl {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @throws InvalidDataException exception thrown if any of the fields are not valid
+     * (either an already existing ID was written, or no image was uploaded)
+     */
     public void validateFormData() throws InvalidDataException {
         for (Activity a : adminInterfaceCtrl.getActivities()) {
             if (a.id.equals(activityId.getText())) {
@@ -143,6 +183,12 @@ public class AddActivityDialogCtrl {
         }
     }
 
+    /**
+     * Internal helper method. Automatically creates a path to the image by parsing the ID.
+     *
+     * @param id the ID of the activity
+     * @return the appropriate path for the image.
+     */
     private String createPath(String id) {
         return "to-be-implemented";
     }
